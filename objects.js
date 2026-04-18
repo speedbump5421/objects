ab70c482d9d31945c686cd538c1521ac803804be
Daniel Morales <speedbump5421@gmail.com>
2026-04-18 14:02:10 -0500

    Update objects.js

--- objects.js
+++ objects.js
@@ -15,3 +15,19 @@ const otherCar = {
 console.log(otherCar);
 
 // This is only a test
+
+const myPrinter = {
+  brand: "HP",
+  series: "OfficeJet",
+  model: "OfficeJet Pro 9015e",
+  color: "White",
+  type: "All-in-One",
+  connectivity: ["Wi-Fi", "USB", "Ethernet"],
+  functions: ["Print", "Scan", "Copy", "Fax"],
+  paperSize: ["Letter", "Legal", "4x6"],
+  printSpeed: "Up to 22 ppm (black)",
+  wireless: true,
+};
+
+console.log(myPrinter);
+
