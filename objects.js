ea7fc0765b9d0e1f6499f3a8558a031e6d028ef4
Daniel Morales <speedbump5421@gmail.com>
2026-04-18 13:47:36 -0500

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,15 @@
+const myCar = {};
+
+myCar.make = "Toyota";
+myCar.model = "Corolla";
+myCar.year = 2020;
+
+console.log(myCar);
+
+const otherCar = {
+  make: "Honda",
+  model: "Civic",
+  year: 2019,
+};
+
+console.log(otherCar);
